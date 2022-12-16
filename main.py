de42e520c426335c00f806790a5a118a444e3004
XXairship <116432782+ipx13@users.noreply.github.com>
2022-12-16 19:46:29 +0800

    edited the queues.py to make some corrections

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from queues import Queue
 
-fifo = Queue()
-fifo.enqueue("1st")
-fifo.enqueue("2nd")
-fifo.enqueue("3rd")
+fifo = Queue("1st", "2nd", "3rd")
+print(len(fifo))
 
-fifo.dequeue()
-fifo.dequeue()
-fifo.dequeue()
+for element in fifo:
+    print(element)
+
+print(len(fifo))
