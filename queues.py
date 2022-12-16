8c15d8339115f4caa72c5ecc0bc903734002f242
XXairship <116432782+ipx13@users.noreply.github.com>
2022-12-16 18:57:02 +0800

    added *elements and special methods such as .__iter__() and .__len__() to make the queue iterable

--- queues.py
+++ queues.py
@@ -1,5 +1,15 @@
 from collections import deque
 
+def __init__(self, *elements):
+    self._elements = deque(elements)
+
+def __len__(self):
+    return len(self._elements)
+
+def __iter__(self):
+    while len(self) > 0:
+        yield self.dequeue()
+
 class Queue:
     def __init__(self):
         self._elements = deque()
