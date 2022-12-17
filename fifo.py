6297245d0473fd381d879a9653773644fb2a8a9a
XXairship <116432782+ipx13@users.noreply.github.com>
2022-12-17 20:56:11 +0800

    Edited lifo.py and fifo.py to add modifications

--- fifo.py
+++ fifo.py
@@ -9,5 +9,19 @@ print("Length of queue (initial):", len(fifo))
 print("\nPrinting each element, First in-First Out:")
 for element in fifo:
     print(">> ", element)
+
 print("\nDequeueing Process:")
 print("Length of queue (final):", len(fifo))
+
+'''
+Paste in the interpreter:
+
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+fifo.dequeue()
+fifo.dequeue()
+fifo.dequeue()
+'''
---
+++ lifo.py
@@ -0,0 +1,26 @@
+from queues import Stack
+
+print("\n----- Last In - First Out Queue (Queue Data Type) -----")
+lifo = Stack("1st", "2nd", "3rd")
+print("\nEnqueueing Process:")
+print("Length of queue (initial):", len(lifo))
+
+print("\nPrinting each element, First in-First Out:")
+for element in lifo:
+    print(element)
+
+print("\nDequeueing Process:")
+print("Length of queue (final):", len(lifo))
+
+'''
+Paste in the interpreter:
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+lifo.pop()
+lifo.pop()
+lifo.pop()
+'''
