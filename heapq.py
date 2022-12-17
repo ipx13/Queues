7646084ad0c32515ae161c0fa99ea0d38503503e
XXairship <116432782+ipx13@users.noreply.github.com>
2022-12-17 22:31:26 +0800

    added contents to heapq.py

--- heapq.py
+++ heapq.py
@@ -1,6 +1,44 @@
+'''
 from heapq import heappush
 
 fruits = []
 heappush(fruits, "orange")
 heappush(fruits, "apple")
 heappush(fruits, "banana")
+
+fruits
+
+------------------------------
+
+from heapq import heappop
+
+heappop(fruits)
+
+fruits
+
+------------------------------
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+
+person2 < person3
+
+--------------------------------------
+
+from queues import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+messages.dequeue()
+'''
